c763a2db855d0c0434a560394c0e07957d766a4c
clutch70 <67942178+clutch70@users.noreply.github.com>
2022-09-23 04:01:09 -0500

    Working Chrome and Win favs

--- FavGrabber.ps1
+++ FavGrabber.ps1
@@ -7,9 +7,16 @@
 #   and cleanly reports them one line at a time.
 #
 #
-#
-#
 ########################################################################
+
+    <#
+    .Synopsis
+        Lists the server names of all favorites found on the device
+    .Description
+        Iterates through all user folders and finds favorites for Chrome
+        and Windows/IE/Edge
+
+    #>
 
 
 # Variables we'll need
@@ -27,10 +34,58 @@ Function cleanUri($uriList)
 {
     foreach ($uri in $uriList)
     {
-        $uri.split('/')[2]
+        #$uri.split('/')[2]
+        # Split up the string based on the / char and grab the 2nd element of that array, the server name
+        $i = $uri.split('/')[2]
+        # Get rid of port numbers and show the output
+        $i.split(':')[0]
     }
 }
 
+function Resolve-ShortcutFile {
+
+    <#
+    https://devblogs.microsoft.com/powershell/resolve-shortcutfile/
+    #>
+    param(
+    [Parameter(
+        ValueFromPipeline=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position = 0)]
+    [Alias("FullName")]
+    [string]
+    $fileName
+    )
+process {
+
+        if ($fileName -like "*.url") {
+            Get-Content $fileName | Where-Object {
+                $_ -like "url=*"
+            } |
+            Select-Object @{
+                Name='ShortcutFile'
+                Expression = {Get-Item $fileName}
+            }, @{
+                Name='Url'
+                Expression = {$_.Substring($_.IndexOf("=") + 1 )}
+            }
+        }
+
+}
+
+}
+
+Function cleanWinFavs($favs)
+{
+    foreach ($fav in $favs)
+    {
+        #$fav
+        $i = $fav | Resolve-ShortcutFile
+        $i.url
+    }
+}
+
+# Capture Chrome favorites
 foreach ($user in $userDirs.name)
 {
     #$target = $usersPath\$user\$chromePath\$chromeFile
@@ -46,6 +101,27 @@ foreach ($user in $userDirs.name)
 
     }
 }
+
+# Capture IE/Windows favorites
+foreach ($user in $userDirs.name)
+{
+    IF (Test-Path $usersPath\$user\Favorites)
+    {
+        $files = gci -Recurse $usersPath\$user\Favorites *.url
+        IF ($files.count -gt 0)
+        {
+            $cleanWin = cleanWinFavs($files)
+            $cleanWin = cleanUri($cleanWin)
+            $cleanWin = $cleanWin | select -Unique
+            $count = $count + $cleanWin.count
+            $list = $list + $cleanWin
+        }
+        #$fileCount = $files.count
+        #$files
+        #Write-Output "Found Windows $fileCount favorites folder for $user!!!"
+    }
+}
+
 
 $list
 $count
